bfc0f476de4339fabefde87d1e0e540ec012f7dc
rakib <mdrakib991943@.com>
2026-04-01 03:07:49 +0600

    added cart Section :

--- src/Components/Carts/Cart/Cart.jsx
+++ src/Components/Carts/Cart/Cart.jsx
@@ -1,9 +1,61 @@
 import React from 'react';
 
-const Cart = () => {
+const Cart = ({ data }) => {
+    console.log(data)
     return (
-        <div>
-            
+        <div >
+            <div className="card w-96 bg-base-100 shadow-xl border border-base-300 overflow-hidden relative">
+               
+                <div className="absolute top-4 right-4">
+                    <div className="badge badge-primary badge-lg font-medium p-4 ">
+                        {data.tag}
+                    </div>
+                </div>
+
+                <div className="flex justify-center pt-8 pb-4">
+                    <div className="w-20 mt-7 h-20 bg-gradient-to-br from-pink-500 via-purple-500 to-yellow-400 rounded-2xl flex items-center justify-center shadow-md">
+                        <img className='w-12' src={data.icon} alt="" />
+                    </div>
+                </div>
+
+                
+                <div className="card-body pt-2 text-center">
+                    <h2 className="card-title text-2xl font-bold text-center justify-center">
+                        {data.name}
+                    </h2>
+
+                    <p className="text-base-content/70 mt-2 text-[15px]">
+                        {data.description}
+                    </p>
+
+                
+                    <div className="mt-6 mb-2">
+                        <span className="text-4xl font-bold">${data.price}</span>
+                        <span className="text-base-content/60 text-lg">/{data.period}</span>
+                    </div>
+
+                    {
+
+                        data.features.map((feature => <div className="flex items-center gap-3">
+                            <div className="text-green-500 text-xl">✓</div>
+                            <span className="text-base-content/80">{feature}</span>
+                        </div>))
+                    }
+
+                    <div className="space-y-3 mt-6 text-left">
+
+
+
+                    </div>
+
+                   
+                    <div className="card-actions mt-8">
+                        <button className="btn btn-primary w-full text-lg font-semibold h-14 rounded-2xl">
+                            Buy Now
+                        </button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Components/Carts/Carts.jsx
+++ src/Components/Carts/Carts.jsx
@@ -1,26 +1,34 @@
 
 import { use } from "react";
+import Cart from "./Cart/Cart";
 
 const Carts = ({ datas }) => {
 
-    const datess = use(datas);
-    console.log(datess)
+    const digitalToolSData = use(datas);
+
     return (
-        <div className="mt-6">
+        <div className="mt-6 w-11/12 mx-auto ">
             <div className="flex justify-center flex-col items-center ">
                 <h1 className="text-4xl font-bold">Premium Digital Tools</h1>
                 <p className="mb-4 mt-3" >Choose from our curated collection of premium digital products designed
                     to boost your productivity and creativity.</p>
                 
-                <div className="tabs tabs-box">
-                    <input  type="radio" name="my_tabs_6" className="tab mr-4 text-white btn bg-[#6107ec]" aria-label="Products" defaultChecked />
+                <div className="tabs tabs-box mt-10">
+                    <input  type="radio" name="my_tabs_6" className="tab text-2xl font-bold
+                     mr-4 text-white btn bg-[#6107ec]" aria-label="Products" defaultChecked />
                     
 
-                    <input type="radio" name="my_tabs_6" className="tab" aria-label="Cart()"  />
+                    <input type="radio" name="my_tabs_6" className="tab text-2xl font-bold" aria-label="Cart()"  />
                     
 
                     
                 </div>
+            </div>
+
+            <div className="grid grid-cols-3 gap-4 mt-10">
+                {
+            digitalToolSData.map((data)=><Cart data={data} key={data.id}></Cart>)
+            }
             </div>
         </div>
     );
